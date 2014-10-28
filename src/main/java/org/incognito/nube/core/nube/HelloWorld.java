69d5cafd0cb1325209e5ce0207313b8a8fceead4
incognito <incognito@INCOGNITO-LAB>
2014-10-27 20:34:53 -0500

    Test New Stuff

--- src/main/java/org/incognito/nube/core/nube/HelloWorld.java
+++ src/main/java/org/incognito/nube/core/nube/HelloWorld.java
@@ -13,12 +13,12 @@ public class HelloWorld {
 
     public static void main(String args[]) {
         System.out.println("Hello World");
-        
-        System.out.println("Hello World");
-                System.out.println("Hello World");
 
-        
-        
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+        System.out.println("Hello World");
+
     }
 ;
 
